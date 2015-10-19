9fbf8c5faf3e030bde9353bff891b109748efd16
Andrew McKenzie <andrew.mckenzie@electronifie.com>
2015-10-19 17:14:17 -0400

    Upgrade to meteor v1.2. Bump version to v1.8.7. Change user to electronifiejs

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
-  "name": "electronifie:pages",
+  "name": "electronifiejs:pages",
   "summary": "State of the art, out of the box Meteor pagination",
-  "version": "1.8.5",
+  "version": "1.8.7",
   "git": "https://github.com/electronifie/meteor-pages"
 });
 
 Package.onUse(function(api){
-    api.versionsFrom("METEOR@1.0.3.1")
+    api.versionsFrom("METEOR@1.2");
     api.use([
         "meteor-platform",
         "check",
@@ -32,14 +32,17 @@ Package.onUse(function(api){
     api.addFiles([
         "client/templates.html",
         "client/controllers.coffee",
-        "client/main.css",
+        "client/main.css"
+    ], "client");
+
+    api.addAssets([
         "public/loader.gif"
     ], "client");
 });
 
 Package.onTest(function(api){
     api.use([
-        "electronifie:pages"
+        "electronifiejs:pages"
     ]);
 
     api.addFiles([
